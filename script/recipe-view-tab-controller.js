ced66e1589150692f48ddb634f7a25fa96ff5273
hinnaweali <hinnaweali@gmail.com>
2024-10-10 11:52:35 +0200

    finish adding illustration

--- script/recipe-view-tab-controller.js
+++ script/recipe-view-tab-controller.js
@@ -1,5 +1,5 @@
 import TabController from "../../../tool/tab-controller.js";
-import {DIET,CATEGORY} from "./enums.js"
+import {DIET,CATEGORY,UNIT} from "./enums.js"
 
 
 /**
@@ -22,13 +22,18 @@ class RecipeViewTabController extends TabController {
 	// HTML element getter operations
 	get querySection () { return this.center.querySelector("section.recipes-query"); }
 	get recipesView () { return this.center.querySelector("section.recipes-view"); }
-	get queryTitelInput() { return this.querySection.querySelector("div.criteria>span.left>div.title>input")}
-	get queryDescription() { return this.querySection.querySelector("div.criteria>span.left>div.description>input")}
-	get queryInstruction() { return this.querySection.querySelector("div.criteria>span.left>div.instruction>input")}
-	get queryIngredientCount() { return this.querySection.querySelector("div.criteria>span.left>div.min-ingredient-count>input")}
-	get categorySelector() { return this.querySection.querySelector("div.criteria>span.left>div.category>select")}
-	get queryDiets() { return this.querySection.querySelector("div.criteria>span.right>div.diets>select")}
-	get queryAuthorship() { return this.querySection.querySelector("div.criteria>span.right>div.authorship>select")}
+	get recipeView () { return this.center.querySelector("section.recipe-view"); }
+
+	get queryTitelInput() { return this.querySection.querySelector("div.criteria>span.left>div.title>input"); }
+	get queryDescription() { return this.querySection.querySelector("div.criteria>span.left>div.description>input"); }
+	get queryInstruction() { return this.querySection.querySelector("div.criteria>span.left>div.instruction>input"); }
+	get queryIngredientCount() { return this.querySection.querySelector("div.criteria>span.left>div.min-ingredient-count>input"); }
+	get categorySelector() { return this.querySection.querySelector("div.criteria>span.left>div.category>select"); }
+	get queryDiets() { return this.querySection.querySelector("div.criteria>span.right>div.diets>select"); }
+	get queryAuthorship () { return this.querySection.querySelector("div.criteria>span.right>div.authorship>select"); }
+	get recipeViewImage () { return this.recipeView.querySelector("div.data>span.avatar>img"); }
+	get recipeViewElement () { return this.recipeView.querySelector("div.ingredients>table>tbody"); }
+
 
 	/**
 	 * Handles that activity has changed from false to true.
@@ -49,7 +54,6 @@ class RecipeViewTabController extends TabController {
 	
 	
 	async processDisplayIngredientsEditor () {
-
 		this.messageOutput.value = "";
 
 		try {
@@ -102,6 +106,7 @@ class RecipeViewTabController extends TabController {
 		}
 	}
 
+
 	async #invokeQueryAllRecipes (title, description, instruction, ingredientCount, category, diets, authored) {
 		const queryFactory = new URLSearchParams();
 		for (const diet of diets) queryFactory.append("diet", diet); 
@@ -123,6 +128,94 @@ class RecipeViewTabController extends TabController {
 
 	async processDisplayIngredientEditor (recipe) {
 		console.log("test1",recipe);
+		this.querySection.classList.add("hidden");
+		this.recipesView.classList.add("hidden");
+
+		const templateRecipeView = document.querySelector("head>template.recipe-view");
+		const tableRow = this.center.append(templateRecipeView.content.firstElementChild.cloneNode(true));
+
+		const imageAvatar = this.sharedProperties["service-origin"] + "/services/documents/" + recipe.avatar.identity;
+		this.recipeViewImage.src = imageAvatar;
+
+		const recipeTitle = this.recipeView.querySelector("div.data>span.other>div.title>input");
+		const recipeDiet = this.recipeView.querySelector("div.data>span.other>div.diet>input");
+		const recipeCategory = this.recipeView.querySelector("div.data>span.other>div.category>input");
+		const recipeDescription = this.recipeView.querySelector("div.description>textarea");
+		const recipeInstruction = this.recipeView.querySelector("div.instruction>textarea");
+
+		recipeTitle.value = recipe.title.trim() || null;
+		recipeDiet.value = DIET[recipe.diet.trim()] || null;
+		recipeCategory.value = CATEGORY[recipe.category.trim()] || null;
+		recipeDescription.value = recipe.description.trim() || null;
+		recipeInstruction.value = recipe.instruction.trim() || null;
+
+		const recipeIngredients = await this.#invokeQueryRecipeIngredients(recipe);
+		const templateRecipeViewRow = document.querySelector("head>template.recipe-view-ingredient-row");
+		for (const ingredient of recipeIngredients){
+			console.log("ingredient",ingredient);
+			const recipeViewRowElement = templateRecipeViewRow.content.firstElementChild.cloneNode(true)
+
+			const buttonImage = recipeViewRowElement.querySelector("td.avatar>img");
+			const alias = recipeViewRowElement.querySelector("td.alias.text");
+			const diet = recipeViewRowElement.querySelector("td.diet.text");
+			const amount = recipeViewRowElement.querySelector("td.amount.number");
+			const unit = recipeViewRowElement.querySelector("td.unit.text");
+			
+			buttonImage.src = this.sharedProperties["service-origin"] + "/services/documents/" + ingredient.victual.avatar.identity;
+			alias.innerText = ingredient.victual.alias || "";
+			diet.innerText = DIET[ingredient.victual.diet] || ""; 
+			amount.innerText = ingredient.amount.toString() || "0";
+			unit.innerText = UNIT[ingredient.unit.toString()] || "0";
+			this.recipeViewElement.append(recipeViewRowElement);
+		}
+
+		const illustrations = await this.#invokeQueryRecipeIllustrations(recipe);
+		const illustrationTableBody = this.recipeView.querySelector("div.illustrations>table>tbody");
+		const illustrationTemplate =   document.querySelector("head>template.recipe-view-illustration-row");
+		for (const illustration of illustrations) {
+			const illustrationRowElement = illustrationTemplate.content.firstElementChild.cloneNode(true);
+			
+			const documentLinkContent = illustrationRowElement.querySelector("td.content.text>a");
+			const size = illustrationRowElement.querySelector("td.size.number");
+			
+			const documentLocation = this.sharedProperties["service-origin"] + "/services/documents/" + illustration.identity;
+			documentLinkContent.href = documentLocation || "";
+			documentLinkContent.innerText = illustration.description || "";
+			size.innerText = illustration.size || ""; 
+			illustrationTableBody.append(illustrationRowElement);
+		}
+
+		const button = this.recipeView.querySelector("div.control>button.cancel");
+		button.addEventListener("click",event => this.processReturnToRecipes());
+	}
+
+
+	async #invokeQueryRecipeIngredients (singleRecipe) {
+		const resource = this.sharedProperties["service-origin"] + "/services/recipes/" + singleRecipe.identity + "/ingredients";
+		const headers = { "Accept": "application/json" };
+
+		const response = await fetch(resource, { method: "GET" , headers: headers, credentials: "include" });
+		if (!response.ok) throw new Error("HTTP " + response.status + " " + response.statusText);
+		return response.json();
+	}
+
+	
+	async #invokeQueryRecipeIllustrations (recipe) {
+		const resource = this.sharedProperties["service-origin"] + "/services/recipes/" + recipe.identity + "/illustrations";
+		console.log("resourceeeeeeeeee",resource);
+		const headers = { "Accept": "application/json" };
+
+		const response = await fetch(resource, { method: "GET" , headers: headers, credentials: "include" });
+		if (!response.ok) throw new Error("HTTP " + response.status + " " + response.statusText);
+		return response.json();
+	}
+
+	async processReturnToRecipes() {
+
+		// Show the Recipes view section
+		this.querySection.classList.remove("hidden");
+		this.recipesView.classList.remove("hidden");
+		this.recipeView.remove();
 
 	}
 }
